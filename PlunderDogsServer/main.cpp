e0ba92acd477d18a682040ef6c29b216411b3ecc
Ryan Swann <RyanSwann1992@gmail.com>
2019-07-13 17:22:23 +0100

    Removed improper code

--- PlunderDogsServer/main.cpp
+++ PlunderDogsServer/main.cpp
@@ -77,7 +77,6 @@ FactionName getAvaiableFactionName(std::array<Faction, static_cast<size_t>(Facti
 
 bool isServerFull(std::array<Faction, static_cast<size_t>(FactionName::eTotal)>& factions)
 {
-	return true;
 	auto cIter = std::find_if(factions.cbegin(), factions.cend(), [](const auto& faction) { return !faction.occupied; });
 	return cIter != factions.cend();
 }
